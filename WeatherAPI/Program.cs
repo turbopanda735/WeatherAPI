ce3b8dd64b3b87ee72b01dac1df1e3cd0ce144d8
Joel <franco4prez@gmail.com>
2022-09-10 00:00:51 -0400

    Add project files.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Web;
+using System;
+using System.Linq;
+using System.IO;
+using WeatherAPI;
+using Microsoft.Extensions.Configuration;
+
+var sr = new StreamReader("appsettings.json");
+var json = sr.ReadToEnd().Split(",");
+var OCKey = json[0].Split(":")[1].Replace("\"", " ").Trim().ToString();
+var weatherKey = json[1].Split(":")[1].Replace("\"", " ").Replace("}", " ").Trim().ToString();
+
+
+var client = new HttpClient();
+var encode = new ASCIIEncoding();
+
+
+Console.WriteLine("Weather for?:");
+var locationAddressDecode = Console.ReadLine();
+var locationAddressEncode = HttpUtility.UrlEncode(locationAddressDecode, encode);
+
+var geoCodingURL = $"https://api.opencagedata.com/geocode/v1/json?q={locationAddressEncode}&key={OCKey}";
+var geoCodingResponse = client.GetStringAsync(geoCodingURL).Result;
+var geoCodeResult = JObject.Parse(geoCodingResponse).SelectToken("results");
+
+var latitudeDMS = geoCodeResult[0]["annotations"]["DMS"]["lat"].ToString();
+var longitudeDMS = geoCodeResult[0]["annotations"]["DMS"]["lng"].ToString();
+
+var latDegrees = Utility.ConvertDegreeAngleToDouble(latitudeDMS);
+var lngDegrees = Utility.ConvertDegreeAngleToDouble(longitudeDMS);
+
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?lat={latDegrees}&lon={lngDegrees}&appid={weatherKey}&units=imperial";
+var weatherResponse = client.GetStringAsync(weatherURL).Result;
+var weatherResult = JObject.Parse(weatherResponse).SelectToken("main").SelectToken("temp").ToString();
+
+Console.WriteLine($"{weatherResult} degrees F");
---
+++ WeatherAPI/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI
+{
+    public static class Utility
+    {
+        public static double ConvertDegreeAngleToDouble(string degreeMinuteSecond)
+        {
+            var degree = degreeMinuteSecond.Split('\u00B0')[0];
+            var minutes = degreeMinuteSecond.Split('\u00B0')[1].Split('\u0027')[0];
+            var seconds = degreeMinuteSecond.Split('\u00B0')[1].Split('\u0027')[1].Split('\u0027')[0];
+
+            if (degreeMinuteSecond.Contains("S") || degreeMinuteSecond.Contains("W"))
+            {
+                return -1 * Math.Round(Convert.ToDouble(degree) + (Convert.ToDouble(minutes) / 60) + (Convert.ToDouble(seconds) / 3600), 2);
+            }
+            else return Math.Round(Convert.ToDouble(degree) + (Convert.ToDouble(minutes) / 60) + (Convert.ToDouble(seconds) / 3600), 2);
+
+            //Decimal degrees = 
+            //   whole number of degrees, 
+            //   plus minutes divided by 60, 
+            //   plus seconds divided by 3600
+            //(minutes / 60) + (seconds / 3600) 
+        }
+    }
+}
