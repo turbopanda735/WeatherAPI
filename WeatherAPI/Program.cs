921450722d448816776e96eee3e49fad5f601690
Joel <franco4prez@gmail.com>
2022-09-12 10:45:38 -0400

    changed stream reader

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -6,11 +6,14 @@ using System.Linq;
 using System.IO;
 using WeatherAPI;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
 
 var sr = new StreamReader("appsettings.json");
-var json = sr.ReadToEnd().Split(",");
-var OCKey = json[0].Split(":")[1].Replace("\"", " ").Trim().ToString();
-var weatherKey = json[1].Split(":")[1].Replace("\"", " ").Replace("}", " ").Trim().ToString();
+var json = sr.ReadToEnd();
+
+
+List<Key> keys = JsonConvert.DeserializeObject<List<Key>>(json);
 
 
 var client = new HttpClient();
@@ -21,23 +24,35 @@ Console.WriteLine("Weather for?:");
 var locationAddressDecode = Console.ReadLine();
 var locationAddressEncode = HttpUtility.UrlEncode(locationAddressDecode, encode);
 
+
 try
 {
-    var geoCodingURL = $"https://api.opencagedata.com/geocode/v1/json?q={locationAddressEncode}&key={OCKey}";
+    var geoCodingURL = $"https://api.opencagedata.com/geocode/v1/json?q={locationAddressEncode}&key={keys[0].ApiKey}";
     var geoCodingResponse = client.GetStringAsync(geoCodingURL).Result;
-    var geoCodeResult = JObject.Parse(geoCodingResponse).SelectToken("results");
+    var geoCodeResult = JObject.Parse(geoCodingResponse);
 
-    var latitudeDMS = geoCodeResult[0]["annotations"]["DMS"]["lat"].ToString();
-    var longitudeDMS = geoCodeResult[0]["annotations"]["DMS"]["lng"].ToString();
+    var latitudeDMS = geoCodeResult["results"][0]["annotations"]["DMS"]["lat"].ToString();
+    var longitudeDMS = geoCodeResult["results"][0]["annotations"]["DMS"]["lng"].ToString();
 
     var latDegrees = Utility.ConvertDegreeAngleToDouble(latitudeDMS);
     var lngDegrees = Utility.ConvertDegreeAngleToDouble(longitudeDMS);
 
-    var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?lat={latDegrees}&lon={lngDegrees}&appid={weatherKey}&units=imperial";
+    var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?lat={latDegrees}&lon={lngDegrees}&appid={keys[1].ApiKey}&units=imperial";
     var weatherResponse = client.GetStringAsync(weatherURL).Result;
-    var weatherResult = JObject.Parse(weatherResponse).SelectToken("main").SelectToken("temp").ToString();
+    var weatherResult = JObject.Parse(weatherResponse);
 
-    Console.WriteLine($"{weatherResult} degrees F");
+    var forecast = weatherResult["weather"][0]["description"].ToString();
+    var currentTemp = weatherResult["main"]["temp"].ToString();
+    var minTemp = weatherResult["main"]["temp_min"].ToString();
+    var maxTemp = weatherResult["main"]["temp_max"].ToString();
+    var country = weatherResult["sys"]["country"].ToString();
+    var city = weatherResult["name"].ToString();
+
+    Console.WriteLine($"{city}, {country}");
+    Console.WriteLine(forecast);
+    Console.WriteLine($"Right now: {currentTemp} degrees F");
+    Console.WriteLine($"Daily high: {maxTemp}");
+    Console.WriteLine($"Daily low: {minTemp}");
 }
 catch (ArgumentOutOfRangeException)
 {
